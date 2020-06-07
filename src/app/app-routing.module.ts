6179ccd76f12b17705ee90be8bf6166b2a6bfdff
Uday Kant <udaydeveloper@gmail.com>
2020-06-08 00:35:55 +0530

    Application commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TeamComponent } from './pages/team/team.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about-us', component:AboutUsComponent},
+  {path:'team', component:TeamComponent},
+  {path:'testimonials', component:TestimonialsComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'portfolio', component:PortfolioComponent},
+  {path:'pricing', component:PricingComponent},
+  {path:'blog', component:BlogComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TeamComponent } from './pages/team/team.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    TeamComponent,
+    TestimonialsComponent,
+    ServicesComponent,
+    PricingComponent,
+    BlogComponent,
+    ContactComponent,
+    PortfolioComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
